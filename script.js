360119018b742b30398018dd83e494bcd92cbdb1
suleman15 <sulemanahmed2704@gmail.com>
2023-11-13 17:11:27 +0500

    contactUsForm

--- script.js
+++ script.js
@@ -5,32 +5,26 @@ let array = [53, 23, 23, 23, 234, 23];
 //===========================================
 // Custom Every
 
-// const customEvery = (arr, every) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (!every(arr[i])) return false;
-//   }
-//   return true;
-// };
-// console.log(customEvery(array, (item) => item >= 5));
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// const customEvery = (arr, every) => {
-//   arr.forEach((item) => {
-//     if (!every(item)) return false;
-//   });
-//   return true;
-// };
-// console.log(customEvery(array, (item) => item >= 5));
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Array.prototype.customEvery = function (every) {
-//   for (let i = 0; i < this.length; i++) {
-//     if (!every(this[i])) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+const customEvery = (arr, every) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (!every(arr[i])) return false;
+  }
+  return true;
+};
+console.log(customEvery(array, (item) => item >= 5));
 
-// console.log(array.customEvery((item) => item > 20));
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Array.prototype.customEvery = function (every) {
+  for (let i = 0; i < this.length; i++) {
+    if (!every(this[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(array.customEvery((item) => item > 20));
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //===========================================
@@ -38,46 +32,50 @@ let array = [53, 23, 23, 23, 234, 23];
 //===========================================
 //===========================================
 // Custom Some
-// const customSome = (arr, some) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (some(arr[i])) return true;
-//   }
-//   return false;
-// };
+const customSome = (arr, some) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (some(arr[i])) return true;
+  }
+  return false;
+};
 
-// console.log(customSome(array, (item) => item > 36));
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Array.prototype.customSome = function (some) {
-//   for (let i = 0; i < this.length; i++) {
-//     if (some(this[i])) {
-//       return true;
-//     }
-//   }
-//   return false;
-// };
-// console.log(array.customSome((item) => item < 23));
+console.log(customSome(array, (item) => item > 36));
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+Array.prototype.customSome = function (some) {
+  for (let i = 0; i < this.length; i++) {
+    if (some(this[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+console.log(array.customSome((item) => item < 23));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 //===========================================
 //===========================================
 //===========================================
+
 //Custom Reduce
-// const customReduce = (arr, reducer, initialized) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     initialized = reducer(initialized, arr[i]);
-//   }
-//   return initialized;
-// };
+const customReduce = (arr, reducer, initialized) => {
+  for (let i = 0; i < arr.length; i++) {
+    initialized = reducer(initialized, arr[i]);
+  }
+  return initialized;
+};
 
-// console.log(customReduce(array, (prev, current) => prev + current, 60));
+console.log(customReduce(array, (prev, current) => prev + current, 60));
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// Array.prototype.customReducer = function (reducer, intializedValue) {
-//   for (let i = 0; i < this.length; i++) {
-//     intializedValue = reducer(intializedValue, this[i]);
-//   }
-//   return intializedValue;
-// };
+Array.prototype.customReducer = function (reducer, intializedValue) {
+  for (let i = 0; i < this.length; i++) {
+    intializedValue = reducer(intializedValue, this[i]);
+  }
+  return intializedValue;
+};
 
-// console.log(array.customReducer((prev, current) => prev + current, 0));
+console.log(array.customReducer((prev, current) => prev + current, 0));
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import {Link} from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Input ({placeholder, id, labelName}) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <>  <label className="text-white p-3"  htmlFor={id}>{labelName} </label>
+    <input type="text" className="p-3 rounded" placeholder={placeholder} id={id}  required/></>
   )
+
 }
 
+function App() {
+  return (
+   <>
+  <div className="container flex justify-center bg-black w-[100%] h-screen items-center">
+  <form className="flex flex-col bg-[dodgerblue] p-10 rounded-md w-[600px]   " onSubmit={(e) => {
+      e.preventDefault()
+    }}>
+
+    <div className="text-center text-2xl text-white font-bold ">CONTACT US</div>
+
+      <Input placeholder='USER' id="userName" labelName="User: "/>
+      <Input placeholder='USER EMAIL' id="Email" labelName="Email: "/>
+
+      <label htmlFor="textField " className="text-white p-3 ">Message: </label>
+      <textarea className="min-h-[300px] p-3 rounded" placeholder="MESSAGE" id="textField" required/>
+      <input type="submit" value="Submit" className="hover:bg-[#325374]  hover:text-white bg-white my-3 p-3 rounded-md" />
+    </form>
+    
+  </div>
+   </>
+  );
+}
 export default App
