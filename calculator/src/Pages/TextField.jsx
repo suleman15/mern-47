c6f123e39f64c98f8921b7f682142ef7a8f7279f
suleman15 <sulemanahmed2704@gmail.com>
2023-11-14 18:55:07 +0500

    functionalityAcieved

--- calculator/src/Pages/TextField.jsx
+++ calculator/src/Pages/TextField.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TextField() {
+    const [password, setPassword] = useState("")
+    const [toggle, setToggle] = useState(false)
   return (
-    <div>TextField</div>
+    <div>
+        <label htmlFor="password">Password: </label>
+        <input type={toggle ? "text" : "password"}   />
+        <input type="button" value={!toggle ? "Show" : "Hide" } onClick={() => setToggle(!toggle)} />
+    </div>
   )
 }
 
